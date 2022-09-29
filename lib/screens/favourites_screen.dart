6204b89e5c543dfeba5105b8201b95ee85cd89d2
JBTG <pochopienantek@gmail.com>
2022-09-30 00:42:08 +0200

    first commit

---
+++ lib/screens/favourites_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+
+class FavouritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavouritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Container(
+        child: Text('You have no favorites yet'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            complexity: favoriteMeals[index].complexity,
+            affordability: favoriteMeals[index].affordability,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../screens/categories_screen.dart';
+import '../screens/favourites_screen.dart';
+import '../widgets/main_drawer.dart';
+import '../models/meal.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+
+  TabsScreen(this.favoriteMeals);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, dynamic>> _pages = [];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    _pages = [
+      {'page': CategoriesScreen(), 'title': 'Categories'},
+      {'page': FavouritesScreen(widget.favoriteMeals), 'title': 'Favourites'}
+    ];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          )
+        ],
+      ),
+    );
+  }
+}
